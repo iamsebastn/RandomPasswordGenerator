f7476a0e98963d6b64eae79fe238fa8133472c1b
Sebastian Wolf <sebastian.wolf298@gmail.com>
2023-01-18 15:39:20 +0100

    created the slider and added the value

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
+const lengthSlider = document.getElementById("pw-length")
+const outputValue = document.getElementById("value-length")
+
+outputValue.textContent = lengthSlider.value
+lengthSlider.addEventListener("input", (e) => {
+    outputValue.textContent = e.target.value
+})
+
 document.querySelector(".pw-btn").addEventListener("click", randomPassword)
 
 function randomPassword() {
@@ -10,7 +18,7 @@ function randomPassword() {
     inputLeft.textContent = ""
     inputRight.textContent = ""
 
-    for(let i = 0; i < 10; i++) {
+    for(let i = 0; i < lengthSlider.value; i++) {
         let randomLeft = Math.floor(Math.random() * characters.length)
         let randomRight = Math.floor(Math.random() * characters.length)
         inputLeft.textContent += characters[randomLeft]
