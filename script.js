b1453d60e6cf96efa35a87d3c123e3a1b6aa03f1
Sebastian Wolf <sebastian.wolf298@gmail.com>
2023-01-19 10:49:23 +0100

    simplified the js code

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-document.querySelector(".pw-btn").addEventListener("click", randomPassword)
+const lengthSlider = document.getElementById("pw-length")
+const outputValue = document.getElementById("value-length")
+
 document.addEventListener("click", (e) => {
+    let passwordField = document.querySelectorAll(".op-txt")
+
     if(e.target.dataset.side === "left") {
         let toCopy = document.querySelector("#input-left").textContent
         let inputElement = document.createElement("input")
@@ -27,14 +31,7 @@ document.addEventListener("click", (e) => {
         alert("The right password has been copied")
     } 
 })
-
-const lengthSlider = document.getElementById("pw-length")
-const outputValue = document.getElementById("value-length")
-
-outputValue.textContent = lengthSlider.value
-lengthSlider.addEventListener("input", (e) => {
-    outputValue.textContent = e.target.value
-})
+document.querySelector(".pw-btn").addEventListener("click", randomPassword)
 
 function randomPassword() {
     let inputLeft = document.querySelector("#input-left")
@@ -51,6 +48,11 @@ function randomPassword() {
         inputRight.textContent += characters[randomRight]
     }
 }
+
+outputValue.textContent = lengthSlider.value
+lengthSlider.addEventListener("input", (e) => {
+    outputValue.textContent = e.target.value
+})
 
 
 
